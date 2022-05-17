178e82410041f790cfcbbcc30140555ad1d39077
Nikhil <nikhil@tartanhq.com>
2022-05-17 20:17:38 +0530

    vercel json header

--- src/pages/Home/Home.view.tsx
+++ src/pages/Home/Home.view.tsx
@@ -1,5 +1,5 @@
 
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import React from "react";
 import MovingLine from "./components/MovingLine";
 import { useHomeContext } from "./utils/context";
@@ -8,6 +8,14 @@ export default function HomeView() {
     const { count } = useHomeContext();
     return (
         <Flex direction={'column'}>
+            <Flex>
+                <Flex w='65%' bgColor={'#f5f2f2'}>
+                    
+                </Flex>
+                <Flex w='35%' boxShadow={'-10px 0px 3px 0px #19191a'}>
+                    <Image src="1.jpeg" w='100%' />
+                </Flex>
+            </Flex>
             <MovingLine />
         </Flex>
     );
