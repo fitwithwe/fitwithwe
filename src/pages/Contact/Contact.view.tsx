854102df0fb3a50b92903b298351b24b39042464
Nikhil <nikhil@tartanhq.com>
2022-05-19 22:31:35 +0530

    vercel json header

--- src/pages/Contact/Contact.view.tsx
+++ src/pages/Contact/Contact.view.tsx
@@ -3,7 +3,6 @@ import { Flex } from "@chakra-ui/react";
 import React from "react";
 import PageHeader from "../../components/PageHeader";
 import ContactSection from "./components/ContactSection";
-import { useContactContext } from "./utils/context";
 
 export default function ContactView() {
     return (
