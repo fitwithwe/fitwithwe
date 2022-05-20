ec31667a06326c1ad5b8ceb9b13213cb94b291e5
Nikhil <nikhil@tartanhq.com>
2022-05-20 10:07:33 +0530

    vercel json header

--- src/pages/FreePlan/components/GetStarted.tsx
+++ src/pages/FreePlan/components/GetStarted.tsx
@@ -1,19 +1,18 @@
 import { ArrowForwardIcon } from "@chakra-ui/icons"
-import { Flex,Text } from "@chakra-ui/react"
+import { Flex, Text } from "@chakra-ui/react"
 import { whatsapp_data } from "../../../utils/whatsapp_data"
 
 export default function GetStarted() {
     return (
         <Flex
-            fontSize={{ base: '12px', sm: '13px', md: '14px', lg: '15px' }}
+            fontSize={{ base: '14px', sm: '16px', md: '18px', lg: '20px' }}
             fontWeight='bold'
             color='white'
-            mt={'20px'}
             cursor={'pointer'}
             bgColor='#3c8599'
-            w={{ base: '120px', sm: '140px', md: '160px', lg: '180px' }}
+            w={{ base: '150px', sm: '170px', md: '190px', lg: '210px' }}
             alignItems={'center'}
-            p={{ base: '2px', sm: '4px', md: '6px', lg: '8px' }}
+            p={{ base: '6px', sm: '7px', md: '9px', lg: '10px' }}
             letterSpacing={{ base: '1px', sm: '2px', md: '2px', lg: '2px' }}
             onClick={() => {
                 window.open(whatsapp_data.number, "_self")
@@ -21,8 +20,8 @@ export default function GetStarted() {
             justifyContent='center'
             transition='0.3s ease-out'
             _hover={{
-                color: 'black',
-                bgColor: 'white',
+                color: 'white',
+                bgColor: 'black',
                 transition: '0.3s ease-in'
             }}
         >
